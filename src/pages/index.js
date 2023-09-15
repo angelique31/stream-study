eef3c2e636880833cabb4cd11419d74326425c7a
angelique_rosin <angie1979.capucine@yahoo.fr>
2023-09-15 22:37:57 +0200

    edit error EPERM

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import EmailBar from "@/components/HomeAuthLinks/EmailBar/EmailBar";
 import LogoLink from "@/components/HomeAuthLinks/LogoLink/LogoLink";
 import SignInButton from "@/components/HomeAuthLinks/SignInButton/SignInButton";
 
-import { LandingContainer } from "./landingPage.styled";
+import { LandingContainer } from "../components/HomeAuthLinks/landingPage.styled";
 
 function landingPage() {
   return (
