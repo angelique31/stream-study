6a06c4f8ab1a51b533e3337b337e2a30bae54d29
angelique_rosin <angie1979.capucine@yahoo.fr>
2023-12-09 18:47:23 +0100

    edit email form

--- src/components/ResetEmail/ResetEmail.jsx
+++ src/components/ResetEmail/ResetEmail.jsx
@@ -1,44 +1,262 @@
 import React from "react";
-import {
-  EmailContainer,
-  Title,
-  Greeting,
-  Instruction,
-} from "./RestEmail.styled";
+import Link from "next/link";
 
 const ResetEmail = ({ username, resetLink }) => {
-  const buttonStyle = {
-    backgroundColor: "#E50914",
-    color: "white",
-    fontWeight: 700,
-    padding: "14px 20px",
-    textAlign: "center",
-    textDecoration: "none",
-    display: "inline-block",
-    fontSize: "14px",
-    border: "none",
-    borderRadius: "4px",
-    cursor: "pointer",
-    width: "50%",
-    marginBottom: "30px",
-  };
-  const title = { width: "70%" };
   return (
-    <EmailContainer>
-      <h1 style={title}>Réinitialisation du mot de passe</h1>
-      <Greeting>Bonjour {username},</Greeting>
-      <Instruction>
-        Réinitialisons votre mot de passe afin que vous puissiez continuer à
-        regarder Netflix.
-      </Instruction>
-      {/* <ResetButton href={resetLink}>Réinitialiser le mot de passe</ResetButton> */}
-      <a href={resetLink} style={buttonStyle}>
-        Réinitialiser le mot de passe
-      </a>
-      <Instruction>
-        {`Si vous n'avez pas demandé à réinitialiser votre mot de passe, nous vous conseillons de consulter les derniers accès au compte afin de détecter toute éventuelle activité inhabituelle.`}
-      </Instruction>
-    </EmailContainer>
+    <table
+      width="100%"
+      border="0"
+      cellPadding="0"
+      cellSpacing="0"
+      style={{ backgroundColor: "#eaeaea", height: "550px" }}
+    >
+      <tbody>
+        <tr>
+          <td align="center" style={{ backgroundColor: "#eaeaea" }}>
+            <table
+              align="center"
+              border="0"
+              cellPadding="0"
+              cellSpacing="0"
+              style={{
+                backgroundColor: "#ffffff",
+                width: "500px",
+                height: "500px",
+              }}
+            >
+              <tbody>
+                <tr>
+                  <td>
+                    {/* Logo et Titre */}
+                    <table width="100%" cellPadding="0" cellSpacing="0">
+                      <tbody>
+                        <tr>
+                          <td
+                            align="left"
+                            style={{
+                              paddingLeft: "40px",
+                              paddingRight: "40px",
+                              paddingTop: "20px",
+                            }}
+                          >
+                            {/* Insérez le logo ici */}
+                          </td>
+                        </tr>
+                        <tr>
+                          <td
+                            align="left"
+                            style={{
+                              paddingLeft: "40px",
+                              paddingRight: "40px",
+                              fontSize: "36px",
+                              fontWeight: "700",
+                              lineHeight: "43px",
+                              paddingTop: "20px",
+                              color: "#221f1f",
+                            }}
+                          >
+                            Réinitialisation du mot de passe
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+
+                    {/* Contenu */}
+                    <table
+                      align="left"
+                      width="100%"
+                      cellPadding="0"
+                      cellSpacing="0"
+                    >
+                      <tbody>
+                        <tr>
+                          <td
+                            align="left"
+                            style={{
+                              paddingLeft: "40px",
+                              paddingRight: "40px",
+                              fontSize: "16px",
+                              lineHeight: "21px",
+                              paddingTop: "20px",
+                              paddingBottom: "20px",
+                              color: "#221f1f",
+                            }}
+                          >
+                            Bonjour {username},
+                          </td>
+                        </tr>
+                        <tr>
+                          <td
+                            align="left"
+                            style={{
+                              paddingLeft: "40px",
+                              paddingRight: "40px",
+                            }}
+                          >
+                            Réinitialisons votre mot de passe afin que vous
+                            puissiez continuer à regarder Netflix.
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+
+                    {/* Bouton de Réinitialisation */}
+                    <table
+                      align="center"
+                      width="100%"
+                      cellPadding="0"
+                      cellSpacing="0"
+                    >
+                      <tbody>
+                        <tr>
+                          <td
+                            align="center"
+                            style={{
+                              paddingLeft: "40px",
+                              paddingRight: "40px",
+                              paddingTop: "20px",
+                            }}
+                          >
+                            <a
+                              href={resetLink}
+                              style={{
+                                backgroundColor: "#e50914",
+                                color: "white",
+                                textDecoration: "none",
+                                padding: "14px 40px",
+                                borderRadius: "4px",
+                                display: "inline-block",
+                              }}
+                            >
+                              Réinitialiser le mot de passe
+                            </a>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+
+                    {/* Texte de suivi */}
+                    <table
+                      align="left"
+                      width="100%"
+                      cellPadding="0"
+                      cellSpacing="0"
+                    >
+                      <tbody>
+                        <tr>
+                          <td
+                            align="left"
+                            style={{
+                              paddingLeft: "40px",
+                              paddingRight: "40px",
+                              fontSize: "16px",
+                              lineHeight: "21px",
+                              paddingTop: "20px",
+                              color: "#221f1f",
+                            }}
+                          >
+                            {`Si vous n'avez pas demandé à réinitialiser votre mot
+                            de passe, nous vous conseillons de consulter `}
+                            <Link
+                              href="http://localhost:3000/auth/login"
+                              style={{ textDecoration: "underline" }}
+                            >
+                              {`les derniers accès au compte `}
+                            </Link>
+                            afin de détecter toute éventuelle activité
+                            inhabituelle.
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+
+                    <table width="100%" cellPadding="0" cellSpacing="0">
+                      <tbody>
+                        <tr>
+                          <td
+                            style={{
+                              paddingLeft: "40px",
+                              paddingRight: "40px",
+                              paddingTop: "30px",
+                            }}
+                          >
+                            <table
+                              align="center"
+                              width="100%"
+                              cellPadding="0"
+                              cellSpacing="0"
+                            >
+                              <tbody>
+                                <tr>
+                                  <td
+                                    style={{
+                                      fontSize: "0",
+                                      lineHeight: "0",
+                                      borderTop: "2px solid #221F1F",
+                                    }}
+                                  >
+                                    {/* Espace décoratif */}
+                                  </td>
+                                </tr>
+                              </tbody>
+                            </table>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+
+                    {/* Informations supplémentaires */}
+                    <table
+                      align="left"
+                      width="100%"
+                      cellPadding="0"
+                      cellSpacing="0"
+                    >
+                      <tbody>
+                        <tr>
+                          <td
+                            align="left"
+                            style={{
+                              paddingLeft: "40px",
+                              paddingRight: "40px",
+                              fontSize: "14px",
+                              lineHeight: "17px",
+                              paddingTop: "20px",
+                              color: "#221f1f",
+                            }}
+                          >
+                            {`L'équipe Netflix`}
+                          </td>
+                        </tr>
+                      </tbody>
+
+                      <tbody>
+                        <tr>
+                          <td align="center" style={{ padding: "20px" }}>
+                            <a
+                              rel="noreferrer"
+                              target="_blank"
+                              href="https://1D9VF.trk.elasticemail.com/tracking/unsubscribe?d=sdUhzIpwcbCINNKiwSxFHZqUv122gz7I7XNPzlbunTz5XUkTcmpJfeMDrI36XiJ_kQoABCVSZTm983l5SopsX2xL7Kuda5XPgCyYwx_VVeuG0"
+                              style={{
+                                textDecoration: "underline",
+                                display: "inline-block",
+                                fontSize: "0.7rem",
+                              }}
+                            >
+                              Se désabonner
+                            </a>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   );
 };
 
